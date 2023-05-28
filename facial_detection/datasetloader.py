e09650e8ed9eaff94bdab0ccc604795e2ff983de
Arnold <rnldlinus@gmail.com>
2023-05-28 16:15:16 +0300

    first commit

---
+++ facial_detection/datasetloader.py
@@ -0,0 +1,59 @@
+import h5py
+import numpy as np
+import sys
+import glob
+import tensorflow as tf #The main deep learning library
+
+class TrainingDatasetLoader(tf.keras.utils.Sequence): # Allows the class to be used as a data generator for training models 
+    def __init__(self,data_path, batch_size=200, training=True):
+        
+        # Accessing the path to the data
+        print(f"Opening {data_path}")
+        sys.stdout.flush() # Ensures that the message is immediately printed in the console
+        self.cache = h5py.File(data_path, "r") # Openning the HDF5 file in read only mode
+        
+        # Loading the dataset
+        print("Loading data...")
+        sys.stdout.flush()
+        self.images = self.cache["images"][:]
+        self.labels = self.cache['labels'][:].astype(np.float32) # Convert the labels into float32 type
+        self.image_dims = self.images.shape # Assigning the shape of the loaded images
+        
+        # Creating the index arrays for the negative and positive samples
+        train_inds = np.arange(len(self.images))
+        pos_train_inds = train_inds[self.labels[train_inds, 0] == 1.0] # Contain indices where the labels are 1
+        neg_train_inds = train_inds[self.labels[train_inds, 0] != 1.0] # Contain indices where the labels are not 1
+        
+        # Splitting the positive and negative indices into a 80:20 ratio for training:non-training
+        if training: # During training, assign 80% of the pos and neg indices
+            self.pos_train_inds = pos_train_inds[:int(.8 * len(pos_train_inds))]
+            self.neg_train_inds = neg_train_inds[:int(.8 * len(neg_train_inds))]
+        else: # During training, assign 20% of the pos and neg indices
+            self.pos_train_inds = pos_train_inds[-1 * int(.2 * len(pos_train_inds))]
+            self.neg_train_inds = neg_train_inds[-1 * int(.2 * len(neg_train_inds))]
+        
+        # Randomizing the training samples in each epoch
+        np.random.shuffle(self.pos_train_inds)
+        np.random.shuffle(self.neg_train_inds)
+        
+        self.train_inds = np.concatenate ((self.pos_train_inds, self.neg_train_inds))
+        self.train_batch_size = batch_size
+        self.p_pos = np.ones(self.pos_train_inds.shape) / len(self.pos_train_inds)
+        
+    def get_batch(self, batch_size):
+        selected_pos_inds = np.random.choice(self.pos_train_inds, size=batch_size // 2, replace=False, p=self.p_pos) # Ensures an equal number of positive samples in the batch
+        selected_neg_inds = np.random.choice(self.neg_train_inds, size=batch_size // 2, replace=False) # Ensures an equal number of negative samples in the batch
+        selected_inds = np.concatenate((selected_pos_inds, selected_neg_inds)) # concatenates the selected positive and negative indices into a single array
+        
+        sorted_inds = np.sort(selected_inds).flatten() # Sorts the selected indices in ascending order
+        sorted_inds = sorted_inds.astype(int) 
+        train_img = (self.images[sorted_inds] / 255.0).astype(np.float32) # Normalizing the images
+        train_label = (self.labels[sorted_inds]).astype(np.float32)
+        
+        return train_img, train_label
+        
+    def get_train_size(self):
+        return len(self.train_inds)
+        
+    
+    
---
+++ facial_detection/utils.py
@@ -0,0 +1,62 @@
+import time
+import numpy as np
+from IPython import display as ipythondisplay
+import matplotlib.pyplot as plt
+
+# LossHistory: Used to track the  evolution of the loss during training
+class LossHistory: 
+    def __init__(self,smoothing_factor=0.0): # No smoothing applied in the data. Each loss appended to the list without any smoothing calculations
+        self.alpha = smoothing_factor 
+        self.loss = [] # Initialize an empty list to append the loss
+    def append(self, value): # Appending the loss value to the loss list
+        self.loss.append(self.alpha*self.loss[-1] + (1-self.alpha)*value if len(self.loss)>0 else value) # the smoothing calculation
+    def get(self):
+        return self.loss # Return the current list of the loss values  
+    
+# PeriodicPlotter: Used to create periodic plots for tge training process
+class PeriodicPlotter:
+    def __init__(self, sec, xlabel='', ylabel='', scale=None):
+        self.xlabel = xlabel
+        self.ylabel = ylabel
+        self.sec = sec
+        self.scale = scale
+        self.tic = time.time()
+    def plot(self, data): # Plots the data
+        if time.time() - self.tic > self.sec: # Checking if the last time interval has passed since the last plot
+            plt.cla() # If yes, the current plot is cleared
+            
+            # Creates a new plot using a specified scale, either none, semilogx, semilogy or loglog
+            if self.scale is None:
+                plt.plot(data)
+            elif self.scale == 'semilogx':
+                plt.semilogx(data)
+            elif self.scale == 'semilogy':
+                plt.semilogy(data)
+            elif self.scale == 'loglog':
+                plot.loglog(data)
+            else: 
+                raise ValueError(f"{self.scale} is not recognized. Please use a recognized one!)")
+                
+            plt.xlabel(self.xlabel), plt.ylabel(self.ylabel)
+            ipythondisplay.clear_output(wait=True) # clears the output displayed in the IPython environment, ensuring that only the current plot is visible
+            ipythondisplay.display(plt.gcf()) # displays the current plot
+            
+            
+            
+    def create_grid_of_images(xs, size=(5,5)): # Combines a list of images into a single grid. xs: List of images. size: desired size of the grid
+        grid = [] # Initalizing an empty list that will house the rows of the image grid
+        counter = 0 # a counter variable to keep track of the position in the xs list        
+       
+        for i in range(size[0]):  # Iterating over the desired number of rows
+            row = [] # Initialize an empty list of rows
+            for j in range(size[1]): #Iterating over the desired number of columns
+                row.append(xs[counter]) # Append the image into the current position in the image list to the row list
+                counter += 1 # Incrementing the counter to move to the next image in the image (xs) list
+            row = np.hstack(row) # Horizontally stack the images creating a single row of images
+            grid.append(row) # Append the single row to the grid list
+        grid = np.vstack(grid) # vertically stack the riws in the grid list into a single grid
+        return grid
+        
+            
+                
+    
